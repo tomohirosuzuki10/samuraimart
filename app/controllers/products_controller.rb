789607859396fdb21b611c158f85264cc9ef7590
tomohirosuzuki10 <tttttomo.1128@gmail.com>
2023-11-24 07:33:03 +0000

    09 create_reviews

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,13 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
+    
+    @reviews = @product.reviews
+    @review = @reviews.new
   end
 
   def new
@@ -19,20 +22,24 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
+    
     @categories = Category.all
   end
 
   def update
-    @product = Product.find(params[:id])
+    
     @product.update(product_params)
     redirect_to product_url @product
   end
 
   def destroy
-    @product = Product.find(params[:id])
+    
     @product.destroy
     redirect_to products_url
+  end
+  
+  def set_product
+       @product = Product.find(params[:id])
   end
   
   private
