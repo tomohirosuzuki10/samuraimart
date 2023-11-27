fa299eb8891b478dd9fe7a88b36f66b55eb594ba
tomohirosuzuki10 <tttttomo.1128@gmail.com>
2023-11-27 06:57:48 +0000

    パスワード変更追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user
-  
+  before_action :authenticate_user!
   def edit
     
   end
@@ -18,6 +18,19 @@ class UsersController < ApplicationController
   def edit_address
     
   end
+  def update_password
+    if password_set?
+      @user.update_password(user_params) 
+      flash[:notice] = "パスワードは正しく更新されました。"
+      redirect_to root_url
+    else
+      @user.errors.add(:password, "パスワードに不備があります。")
+      render "edit_password"
+    end
+  end
+  
+  def edit_password
+  end
   
   private
   
@@ -27,6 +40,11 @@ class UsersController < ApplicationController
     
     def user_params
      params.permit(:name, :email, :address, :phone, :password, :password_confirmation)
+    end
+    
+    def password_set?
+      user_params[:password].present? && user_params[:password_confirmation].present? ?
+      true : false
     end
     
 end
